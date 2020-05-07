4be6b79856a1fb63f6b346c701b41766e3bbd88f
axs5489 <32942498+axs5489@users.noreply.github.com>
2020-05-07 19:32:20 -0400

    Update win.py

--- Utilities/win.py
+++ Utilities/win.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 07 17:35:39 2020
+
+@author: asasson
+"""
+
+import os
 import sys
 import ctypes
 import glob
@@ -7,16 +15,55 @@ import shutil
 import time
 import win32api
 
+
+def timeExpired(startTime, numSec=45):
+	""" Checks if a certain number of seconds has passed since startTime"""
+	return ((time.perf_counter() - startTime) < numSec)
+	
+def validatePort(tport, debugOn = False):
+	""" Given a port, validate if connected"""
+	if debugOn : print("{}".format(tport))
+	try:
+		p = serial.Serial(port = tport,baudrate = 115200,timeout = 3)
+		if debugOn : print(p.port," ",p.isOpen())
+		buf = ""
+		for i in range(10):
+			try:
+				p.write("\n".encode('utf-8'))
+				p.write(b'ascii\r')
+				buf += Rx.readline()
+				if(buf.find("\n") or buf.find("\r")):
+					break
+			except:
+				if debugOn : print("Serial Error")
+			time.sleep(5)
+		p.close()
+		return True
+	except serial.SerialException:
+		if debugOn : print(tport, " already open")
+		if(p.isOpen()) :
+			p.close()
+		return -1
+	except:
+		print("ERROR")
+		if(p.isOpen()) :
+			p.close()
+		return -2
+	return False
+
 def listSerialPorts():
-	""" Cross-platform available serial port lister """
+	""" Cross-platform available serial port lister"""
 	if sys.platform.startswith('win'):
-		ports = ['COM' + str()]
+		ports = ['COM' + str(i + 1) for i in range(256)]
 	elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+		# this is to exclude your current terminal "/dev/tty"
 		ports = glob.glob('/dev/tty[A-Za-z]*')
 	elif sys.platform.startswith('darwin'):
+		import glob
 		ports = glob.glob('/dev/tty.*')
-	else: raise EnvironmentalError('Unsupported platform')
-	
+	else:
+		raise EnvironmentError('Unsupported platform')
+
 	result = []
 	for port in ports:
 		try:
@@ -24,13 +71,13 @@ def listSerialPorts():
 			s.close()
 			result.append(port)
 		except (OSError, SerialException):
-			print("")
+			pass
 	return result
 	
 def driveReady(drive):
-	""" Validates a drive """
+	""" Validates a Drive"""
 	returnValue = 0
-	oldError = win32api.SetErrorMode(1)
+	oldError = win32api.SetErrorMode( 1 )
 	try:
 		win32api.GetVolumeInformation(drive)
 	except:
@@ -42,11 +89,12 @@ def driveReady(drive):
 	return returnValue
 
 def getDrives():
-	""" Generates a list of volume drives """
-	# Generate list of active drive letters
-	drives = win32.GetLogicalDriveStrings().split('\000')[:-1]
+	""" Generates a list of volume names"""
+	# First, let's make a list of all active drive letters
+	drives = win32api.GetLogicalDriveStrings().split('\000')[:-1]
 	
-	list = [drive for drive in drives if driveReady(drive)] # Parse drives and return list of volume names
+	# Now, let's parse the list and generate a list of volume names
+	list = [drive for drive in drives if driveReady(drive)]
 	return list
 
 def getDriveVolumeName(driveLetter):
@@ -57,9 +105,9 @@ def getDriveVolumeName(driveLetter):
 	max_component_len = None
 	file_system_flags = None
 	rc = ctypes.windll.kernel32.GetVolumeInformationW(ctypes.c_wchar_p(driveLetter),
-		volumeNameBuffer, ctypes,sizeof(volumeNameBuffer),
+		volumeNameBuffer, ctypes.sizeof(volumeNameBuffer),
 		serial, max_component_len, file_system_flags,
-		filesystemNameBuffer, ctypes,sizeof(filesystemNameBuffer)
+		filesystemNameBuffer, ctypes.sizeof(filesystemNameBuffer)
 	)
 	return volumeNameBuffer
 
